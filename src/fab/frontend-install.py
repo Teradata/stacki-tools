2df6b8a3e5c617f4bfe5cc86df8e764a91c399ff
Bill Sanders <billysanders@gmail.com>
2017-05-23 09:25:08 -0700

    only search for the stacki repo in --stacki-iso, but search for any repo in --extra-iso(s). fixes stacki bug #172

--- src/fab/frontend-install.py
+++ src/fab/frontend-install.py
@@ -49,44 +49,56 @@ def generate_multicast():
 	d = random.randrange(1,255)
 	return str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d)
 
-def repoconfig(stacki_iso, extra_iso):
-	if extra_iso:
+def find_repos(iso, stacki_only = False):
+	''' supports jumbo pallets as well as not blowing up on stackios '''
+
+	if iso[0] == '/':
+		mountdir = os.path.join('/run', iso[1:])
+	else:
+		mountdir = os.path.join('/run', iso)
+
+	mount(iso, mountdir)
+
+	repodirs = []
+
+	search_dir = mountdir
+	if stacki_only:
+		# if stacki_only, go straight to that directory
+		search_dir = os.path.join(mountdir, 'stacki')
+
+	for (path, dirs, files) in os.walk(search_dir):
+		if 'repodata' in dirs:
+			repodirs.append(path)
+
+	return repodirs
+
+def repoconfig(stacki_iso, extra_isos):
+	# we only want to pull stacki from 'stacki_iso'
+	# but we'll look for all pallets in 'extra_isos'
+	if extra_isos:
 		#
-		# we are going to use the ISO(s) described in the 'extra_iso'
+		# we are going to use the ISO(s) described in the 'extra_isos'
 		# list, so let's move the CentOS repo files out of the way.
 		#
 		subprocess.call(['mkdir', '-p', '/etc/yum.repos.d/save'])
 		subprocess.call(['mv', '/etc/yum.repos.d/*.repo',
 			'/etc/yum.repos.d/save/'])
 
-	file = open('/etc/yum.repos.d/stack.repo', 'w')
+	count = 0
+	repos = find_repos(stacki_iso, stacki_only=True)
+	for iso in extra_isos:
+		repos.extend(find_repos(iso))
 
-	for iso in [ stacki_iso ] + extra_iso:
-		if iso[0] == '/':
-			mountdir = os.path.join('/run', iso[1:])
-		else:
-			mountdir = os.path.join('/run', iso)
-				
-		mount(iso, mountdir)
+	with open('/etc/yum.repos.d/stack.repo', 'w') as repofile:
+		for repo in repos:
+			count += 1
+			reponame = "iso_repo_%s" % count
+			repofile.write('[%s]\n' % reponame)
+			repofile.write('name=%s\n' % reponame)
+			repofile.write('baseurl=file://%s\n' % (repo))
+			repofile.write('assumeyes=1\n')
+			repofile.write('gpgcheck=no\n\n')
 
-		repodir = None
-		for (r, d, f) in os.walk(mountdir):
-			if 'repodata' in d:
-				repodir = r
-				break
-
-		if not repodir:
-			continue
-
-		file.write('[%s]\n' % os.path.basename(iso))
-		file.write('name=%s\n' % os.path.basename(iso))
-		file.write('baseurl=file://%s\n' % (repodir))
-		file.write('assumeyes=1\n')
-		file.write('gpgcheck=no\n\n')
-
-	file.close()
-
-	
 def ldconf():
 	file = open('/etc/ld.so.conf.d/foundation.conf', 'w')
 	file.write('/opt/stack/lib\n')
@@ -125,14 +137,14 @@ opts, args = getopt.getopt(sys.argv[1:], '', [
 	'extra-iso=', 'noX' ]) 
 
 stacki_iso = None
-extra_iso = []
+extra_isos = []
 noX = 0
 
 for opt, arg in opts:
 	if opt == '--stacki-iso':
 		stacki_iso = arg
 	elif opt == '--extra-iso':
-		extra_iso = arg.split(',')
+		extra_isos = arg.split(',')
 	elif opt == '--noX':
 		noX = 1
 
@@ -145,7 +157,7 @@ if not os.path.exists(stacki_iso):
 	print("Error: File '{0}' does not exist.".format(stacki_iso))
 	sys.exit(1)
 
-for iso in extra_iso:
+for iso in extra_isos:
 	if not os.path.exists(iso):
 		print("Error: File '{0}' does not exist.".format(iso))
 		sys.exit(1)
@@ -158,7 +170,7 @@ subprocess.call(['service', 'NetworkManager', 'stop'])
 stacki_iso = os.path.abspath(stacki_iso)
 
 # create repo config file
-repoconfig(stacki_iso, extra_iso)
+repoconfig(stacki_iso, extra_isos)
 
 pkgs = [ 'stack-command', 'foundation-python', 'stack-pylib',
 	'foundation-python-xml', 'foundation-redhat', 
@@ -241,7 +253,7 @@ subprocess.call(['sh', '/tmp/run.sh'])
 
 banner("Adding Pallets")
 subprocess.call([stackpath, 'add', 'pallet', stacki_iso])
-for iso in extra_iso:
+for iso in extra_isos:
 	iso = os.path.abspath(iso)
 	subprocess.call([stackpath, 'add', 'pallet', iso])
 subprocess.call([stackpath, 'enable', 'pallet', '%'])
