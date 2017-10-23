033f645567fa5f262174289a09b9030daf99badc
Bill Sanders <billysanders@gmail.com>
2017-10-23 16:44:40 -0700

    frontendinstall.py failing on some package conflicts, ignore them for now

--- src/fab/frontend-install.py
+++ src/fab/frontend-install.py
@@ -534,11 +534,22 @@ p = subprocess.Popen(cmd, stdin=infile,
 	stdout=outfile)
 rc = p.wait()
 infile.close()
-outfile.close()
 
 if rc:
 	print ("Could not process XML")
 	sys.exit(rc)
+
+ignored_pkgs = ['rpm-build', 'grub2', 'grub2-x86_64-efi', 'shim']
+outfile.seek(0)
+lines = outfile.readlines()
+outfile.close()
+
+with open("/tmp/run.sh", "w") as newout:
+	for line in lines:
+		if line.startswith('zypper install -f -y'):
+			for pkg in ignored_pkgs:
+				line = line.replace(' {0} '.format(pkg), ' ')
+		newout.write(line)
 
 banner("Run Setup Script")
 # run run.sh
